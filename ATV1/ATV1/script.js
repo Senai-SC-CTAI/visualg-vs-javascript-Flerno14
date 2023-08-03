457a2682ec69aa2b3668bf657afedfe9f7049f69
Guilherme Melnik Trombetta <126004347+Flerno14@users.noreply.github.com>
2023-08-03 12:29:55 -0300

    Add files via upload

---
+++ ATV1/ATV1/script.js
@@ -0,0 +1,17 @@
+function Parilidade(){
+    if(Número = 2){
+        alert("Par")
+    }
+    if(Número = 1){
+        alert("Impar")
+    }
+}
+function Número(){
+    var Número = getElementById("Número").value
+
+    var i
+    for (let i = 0; i < array.length; i++){
+        Número = Número + i + ","
+
+    }
+}
